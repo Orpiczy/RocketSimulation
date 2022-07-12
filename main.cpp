f29bb5cd31bcf2e9b19446d5cdec7f1fe2ed1c21
Lukas <orpiczymejl@gmail.com>
2022-07-12 21:38:59 +0200

    Cleans main.cpp and adds gif with gameplay preview

--- main.cpp
+++ main.cpp
@@ -4,19 +4,5 @@
 int main() {
   SimulationCore simcore;
   simcore.run();
-  // SimMainWindow mainWindow;
-  // mainWindow.start();
   return 0;
 }
-
-// #include <iostream>
-// #include <filesystem>
-// namespace fs = std::filesystem;
-// fs::current_path("/home/black_wraith/Documents//AIR/S8/RealTime/packt/Beginning-Cpp-Game-Programming-Second-Edition/Chapter07");
-// std::cout << "Current path is " << fs::current_path() << '\n';
-
-/* TODO:
-1. Jak w cmaku dodac pliki by uproscic sciezke includow
-2. Ze Switchy w CommandWindow zrob liste
-
-*/
